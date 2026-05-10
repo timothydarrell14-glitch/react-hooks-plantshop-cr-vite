f698339506d72d2b2d522f2dbb81ff528538226f
timothydarrell14-glitch <timothydarrell14@gmail.com>
2026-05-10 11:17:55 +0300

    Fix availability state management and update button click handlers in PlantCard component

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -2,17 +2,24 @@ import React, {useState} from "react";
 
 function PlantCard({id, name, price, image, key}) {
 
-  const[availability, setAvailability] = useState(false)
+  const[availability, setAvailability] = useState(true)
+
+  function handleClick(){
+    setAvailability(false)
+  }
+  function handleEvent(){
+    setAvailability(true)
+  }
 
   return (
     <li key={key} className="card" data-testid="plant-item">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {availability ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleEvent}>Out of Stock</button>
       )}
     </li>
   );
